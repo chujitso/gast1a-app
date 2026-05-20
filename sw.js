ca448af92213947b9d48ca4ff4eea405077cfd4c
chujitso <chujitso@outlook.com>
2026-05-20 12:35:45 -0500

    Agregando logica de parche y retirados

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gast1a-v4'; // <-- CAMBIAMOS ESTE NÚMERO/VERSIÓN
+const CACHE_NAME = 'gast1a-v5'; // <-- CAMBIAMOS ESTE NÚMERO/VERSIÓN
 const urlsToCache = [
   '/',
   '/index.html',
